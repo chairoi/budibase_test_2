a760301dab4da0099831c53c8191fb06c3b8cddc
신채원 <87516785+chairoi@users.noreply.github.com>
2023-11-28 02:24:01 +0900

    Update Component.svelte

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,7 +1,7 @@
 <script>
   import { getContext } from "svelte"
 
-  export let text
+  export let text = "test2"
 
   const { styleable } = getContext("sdk")
   const component = getContext("component")
